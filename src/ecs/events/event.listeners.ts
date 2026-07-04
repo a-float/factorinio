5948c5b306359927a764504388809385d4abbf08
Mateusz Zalewski <mati852456@gmail.com>
2026-07-04 12:46:34 +0200

    Do not register drag as click

--- src/ecs/events/event.listeners.ts
+++ src/ecs/events/event.listeners.ts
@@ -1,7 +1,15 @@
+const DRAG_THRESHOLD = 5;
+
 export function registerListeners() {
   const queue = window.world.getResource("eventQueue").userEvents;
+  let mouseDownPos: { x: number; y: number } | null = null;
 
   window.addEventListener("click", (event) => {
+    if (wasDrag(mouseDownPos, { x: event.clientX, y: event.clientY })) {
+      mouseDownPos = null;
+      return;
+    }
+
     queue.push({
       type: "click",
       payload: {
@@ -12,6 +20,13 @@ export function registerListeners() {
     });
   });
 
+  window.addEventListener("mousemove", (event) => {
+    queue.push({
+      type: "mousemove",
+      payload: { x: event.clientX, y: event.clientY },
+    });
+  });
+
   window.addEventListener("keydown", (event) => {
     queue.push({
       type: "keypress",
@@ -19,10 +34,18 @@ export function registerListeners() {
     });
   });
 
-  window.addEventListener("mousemove", (event) => {
-    queue.push({
-      type: "mousemove",
-      payload: { x: event.clientX, y: event.clientY },
-    });
+  window.addEventListener("mousedown", (e) => {
+    mouseDownPos = { x: e.clientX, y: e.clientY };
   });
+}
+
+function wasDrag(
+  mouseDownPos: { x: number; y: number } | null,
+  mouseUpPos: { x: number; y: number },
+) {
+  if (!mouseDownPos) return false;
+  return (
+    Math.abs(mouseDownPos.x - mouseUpPos.x) > DRAG_THRESHOLD ||
+    Math.abs(mouseDownPos.y - mouseUpPos.y) > DRAG_THRESHOLD
+  );
 }
